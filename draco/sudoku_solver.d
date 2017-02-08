0ce66ccf733fc780645e956db2d8e440ba8f182f
matburton <matburton@gmail.com>
2017-02-08 21:49:34 +0000

    Implemented grid splitting and grid lists
    We can now find sudoku solutions, but very slowly because we're missing
    1/3 of the algorithm

--- draco/sudoku_solver.d
+++ draco/sudoku_solver.d
@@ -2,6 +2,12 @@
 #sudoku_printer.g
 #drinc:util.g
 
+type GridList_t = struct
+{
+    *Grid_t     gl_pGrid;
+    *GridList_t gl_pNext;
+};
+
 extern removePossibilityAt(*Grid_t pGrid; uint x, y, value) void;
 
 proc removePossibilitiesRelatedTo(*Grid_t pGrid; uint x, y, value) void:
@@ -33,7 +39,6 @@ proc removePossibilityAt(*Grid_t pGrid; uint x, y, value) void:
     hadPossibility := squareHasPossibility(pGrid, x, y, value);
     removeSquarePossibility(pGrid, x, y, value);
     if hadPossibility then
-        write("-", value, "@", x, ",", y, ";");
         certainValue := getSquareValue(pGrid, x, y);
         if certainValue ~= 0 then
             removePossibilitiesRelatedTo(pGrid, x, y, certainValue);
@@ -42,7 +47,6 @@ proc removePossibilityAt(*Grid_t pGrid; uint x, y, value) void:
 corp;
 
 proc setValueAt(*Grid_t pGrid; uint x, y, value) void:
-    write("+", value, "@", x, ",", y, ";");
     setSquareValue(pGrid, x, y, value);
     removePossibilitiesRelatedTo(pGrid, x, y, value);
 corp;
@@ -80,22 +84,114 @@ proc isPossible(*Grid_t pGrid) bool:
     true
 corp;
 
+proc isComplete(*Grid_t pGrid) bool:
+    uint indexX, indexY;
+    for indexY from 0 upto pGrid*.g_dimension - 1 do
+        for indexX from 0 upto pGrid*.g_dimension - 1 do
+            if getPossibilityCountAt(pGrid, indexX, indexY) ~= 1 then
+                return false;
+            fi;
+        od;
+    od;
+    true
+corp;
+
+proc freeGridList(*GridList_t pGridList) void:
+    *GridList_t pNext;
+    while pGridList ~= nil do
+        pNext := pGridList*.gl_pNext;
+        freeGrid(pGridList*.gl_pGrid);
+        free(pGridList*.gl_pGrid);
+        free(pGridList);
+        pGridList := pNext;
+    od;
+corp;
+
+proc splitGrid(*Grid_t pGrid) *GridList_t:
+    uint indexX, indexY, bestX, bestY, bestCount, count;
+    *GridList_t pGridList, pNext;
+    bestCount := 0;
+    pGridList := nil;
+    for indexY from 0 upto pGrid*.g_dimension - 1 do
+        for indexX from 0 upto pGrid*.g_dimension - 1 do
+            count := getPossibilityCountAt(pGrid, indexX, indexY);
+            if count > bestCount then
+                bestCount := count;
+                bestX := indexX;
+                bestY := indexY;
+            fi;
+        od;
+    od;
+    for count from pGrid*.g_dimension downto 1 do
+        if squareHasPossibility(pGrid, bestX, bestY, count) then
+            pNext := new(GridList_t);
+            if pNext = nil then
+                return pGridList;
+            fi;
+            pNext*.gl_pGrid := new(Grid_t);
+            if pNext*.gl_pGrid = nil then
+                free(pNext);
+                return pGridList;
+            fi;
+            if not cloneGrid(pGrid, pNext*.gl_pGrid) then
+                free(pNext*.gl_pGrid);
+                free(pNext);
+                return pGridList;
+            fi;
+            setValueAt(pNext*.gl_pGrid, bestX, bestY, count);
+            pNext*.gl_pNext := pGridList;
+            pGridList := pNext;
+        fi;
+    od;
+    pGridList
+corp;
+
+proc mergeLists(*GridList_t pFrontList, pBackList) *GridList_t:
+    *GridList_t pNext;
+    pNext := pFrontList;
+    while pNext*.gl_pNext ~= nil do
+        pNext := pNext*.gl_pNext;
+    od;
+    pNext*.gl_pNext := pBackList;
+    pFrontList
+corp;
+
 proc main() void:
-    Grid_t grid;
-    *char pGridString;
+    *char pString;
+    *GridList_t pGridList, pNext;
     MerrorSet(true);
-    if setupGrid(&grid, 2) then
-        setValueAt(&grid, 0, 0, 1);
-        writeln("");
-        setValueAt(&grid, 1, 0, 2);
-        writeln("");
-        setValueAt(&grid, 2, 0, 3);
-        pGridString := getGridString(&grid);
-        writeln("");
-        writeln(pGridString);
-        Mfree(pGridString, CharsLen(pGridString) + 1);
-        writeln("isPossible: ", pretend(isPossible(&grid), byte));
-        freeGrid(&grid);
-        writeln("Done");
+    pGridList := new(GridList_t);
+    if pGridList = nil then
+        return;
     fi;
+    pGridList*.gl_pNext := nil;
+    pGridList*.gl_pGrid := new(Grid_t);
+    if pGridList*.gl_pGrid = nil then
+        free(pGridList);
+        return;
+    fi;
+    if not setupGrid(pGridList*.gl_pGrid, 2) then
+        free(pGridList*.gl_pGrid);
+        free(pGridList);
+    fi;
+    writeln("");
+    while pGridList ~= nil do
+        pNext := pGridList;
+        pGridList := pGridList*.gl_pNext;
+        if isComplete(pNext*.gl_pGrid) then
+            pString := getGridString(pNext*.gl_pGrid); 
+            writeln(pString);
+            writeln("");
+            Mfree(pString, CharsLen(pString) + 1);
+        else
+            if isPossible(pNext*.gl_pGrid) then
+                pGridList := mergeLists(splitGrid(pNext*.gl_pGrid),
+                                        pGridList);
+            fi;
+        fi;
+        freeGrid(pNext*.gl_pGrid);
+        free(pNext*.gl_pGrid);
+        free(pNext);
+    od;
+    writeln("No further solutions");
 corp;
