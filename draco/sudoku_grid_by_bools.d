a68da9a613df6209d97be2d50d36c1b30e5a53a8
matburton <matburton@gmail.com>
2017-03-30 22:59:30 +0100

    Bit-packed squares to reduce memory
    And this also caused a speed-up even though less is in-lined and there
    are more instructions on red-routes.

--- draco/sudoku_grid_by_bools.d
+++ draco/sudoku_grid_by_bools.d
@@ -15,10 +15,12 @@ type GridCache_t = struct
 
 type SquareCache_t = struct
 {
-    ushort sc_possibilityCount;
+    uint sc_possibilityCount;
 };
 
 uint SQUARES_OFFSET = sizeof(Grid_t) + sizeof(GridCache_t);
+
+uint BITS_PER_PACK = sizeof(uint) * 8;
 
 proc getSquarePointer(*Grid_t pGrid; uint x, y) *SquareCache_t:
     pretend(pGrid + SQUARES_OFFSET, *SquareCache_t)
@@ -31,14 +33,12 @@ proc createGrid(uint sectorDimension) *Grid_t:
     *Grid_t pGrid;
     *GridCache_t pGridCache;
     *SquareCache_t pSquareCache;
-    if sizeof(bool) ~= sizeof(byte) then
-        error("Grid implementation needs sizeof(bool) = sizeof(byte)");
-    fi;
     if sectorDimension > 5 then /* We currently overflow pointers */
         return nil;             /* with sector size 6 or greater */
     fi;                         /* and bigger types slows it down */
     dimension := sectorDimension * sectorDimension;  
-    squareSize := sizeof(SquareCache_t) + dimension;
+    squareSize := sizeof(SquareCache_t)
+                + (((dimension - 1) / BITS_PER_PACK) + 1) * sizeof(uint);
     totalSize := sizeof(Grid_t) + sizeof(GridCache_t)
                + dimension * dimension * squareSize;
     pGrid := pretend(Malloc(totalSize), *Grid_t);
@@ -53,16 +53,16 @@ proc createGrid(uint sectorDimension) *Grid_t:
     pGridCache*.gc_totalSize := totalSize;
     pGridCache*.gc_squareSize := squareSize;
     pGridCache*.gc_impossibleSquares := 0;
-    pGridCache*.gc_incompleteSquares := dimension * dimension;    
+    pGridCache*.gc_incompleteSquares := dimension * dimension;
     BlockFill(pGrid + SQUARES_OFFSET,
               totalSize - SQUARES_OFFSET,
-              pretend(true, byte));
+              0xff);
     for y from 0 upto dimension - 1 do
         for x from 0 upto dimension - 1 do          
             getSquarePointer(pGrid, x, y)*.sc_possibilityCount := dimension;
         od;
     od;
-    gridsInMemory := gridsInMemory + 1;
+    gridsInMemory := gridsInMemory + 1;  
     pGrid
 corp;
 
@@ -92,7 +92,7 @@ proc freeGrid(*Grid_t pGrid) void:
 corp;
 
 proc setSquareValue(*Grid_t pGrid; uint x, y, value) void:
-    *bool pSquare;
+    *uint pSquare;
     *SquareCache_t pSquareCache@pSquare;
     *GridCache_t pGridCache;
     pSquareCache := getSquarePointer(pGrid, x, y);
@@ -102,30 +102,36 @@ proc setSquareValue(*Grid_t pGrid; uint x, y, value) void:
     elif pSquareCache*.sc_possibilityCount > 1 then
         pGridCache*.gc_incompleteSquares := pGridCache*.gc_incompleteSquares - 1;
     fi;
-    pSquareCache*.sc_possibilityCount := 1;   
+    pSquareCache*.sc_possibilityCount := 1;
     BlockFill(pSquareCache + sizeof(SquareCache_t),
-              pGrid*.g_dimension,
-              pretend(false, byte));
-    (pSquare + sizeof(SquareCache_t) + value - 1)* := true;
+              pGridCache*.gc_squareSize - sizeof(SquareCache_t),
+              0x0);
+    pSquare := pSquare + sizeof(SquareCache_t)
+             + ((value - 1) / BITS_PER_PACK) * sizeof(uint);
+    pSquare* := pSquare* | (pretend(1, uint) << ((value - 1) % BITS_PER_PACK));
 corp;
 
 proc squareHasPossibility(*Grid_t pGrid; uint x, y, value) bool:
     /* Inlined getSquarePointer */
-    (pretend(pGrid + SQUARES_OFFSET, *bool)
+    (pretend(pGrid + SQUARES_OFFSET, *uint)
      + (y * pGrid*.g_dimension + x)
      * pretend(pGrid + sizeof(Grid_t), *GridCache_t)*.gc_squareSize
-     + sizeof(SquareCache_t) + value - 1)*
+     + sizeof(SquareCache_t)
+     + ((value - 1) / BITS_PER_PACK) * sizeof(uint))*
+    & (pretend(1, uint) << ((value - 1) % BITS_PER_PACK)) ~= 0
 corp;
 
 proc removeSquarePossibility(*Grid_t pGrid; uint x, y, value) boid:
-    *bool pSquare;
-    *SquareCache_t pSquareCache@pSquare;  
-    *GridCache_t pGridCache;
+    *uint pSquare;
+    *SquareCache_t pSquareCache;
+    *GridCache_t pGridCache;  
     pSquareCache := getSquarePointer(pGrid, x, y);
-    if not (pSquare + sizeof(SquareCache_t) + value - 1)* then
+    if not squareHasPossibility(pGrid, x, y, value) then
         return false;
     fi;
-    (pSquare + sizeof(SquareCache_t) + value - 1)* := false;
+    pSquare := pretend(pSquareCache, *uint) + sizeof(SquareCache_t)
+             + ((value - 1) / BITS_PER_PACK) * sizeof(uint);
+    pSquare* := pSquare* & ~(pretend(1, uint) << ((value - 1) % BITS_PER_PACK));
     pSquareCache*.sc_possibilityCount := pSquareCache*.sc_possibilityCount - 1;
     pGridCache := pretend(pGrid + sizeof(Grid_t), *GridCache_t);
     if pSquareCache*.sc_possibilityCount = 0 then
@@ -142,16 +148,14 @@ proc getPossibilityCount(*Grid_t pGrid; uint x, y) uint:
 corp;
 
 proc getSquareValue(*Grid_t pGrid; uint x, y) uint:
-    *bool pSquare;
-    *SquareCache_t pSquareCache@pSquare;  
+    *SquareCache_t pSquareCache;  
     uint possibility;
     pSquareCache := getSquarePointer(pGrid, x, y);
     if pSquareCache*.sc_possibilityCount ~= 1 then
         return 0;
     fi;
-    pSquare := pSquare + sizeof(SquareCache_t);
     for possibility from 0 upto pGrid*.g_dimension - 2 do
-        if (pSquare + possibility)* then
+        if squareHasPossibility(pGrid, x, y, possibility + 1) then
             return possibility + 1;
         fi;
     od;
