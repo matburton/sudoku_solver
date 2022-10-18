bf9610edb24938afc339baa69af8427b15468a38
matburton <mat.burton@red-gate.com>
2022-10-18 22:31:44 +0100

    ASM: Grid_setSquareValue with 'macro'
    Which is just an include, but might be a useful technique

--- mega_processor/asm/Grid.asm
+++ mega_processor/asm/Grid.asm
@@ -2,10 +2,14 @@
 SQUARE_COUNT equ 81;
 GRID_SIZE    equ SQUARE_COUNT * 4 + 2;
 
+// function int gridByteSize()
+//
 Grid_gridByteSize:
         ld.w r1, #GRID_SIZE;
         ret;
         
+// function Array initialise(Array grid)
+//
 Grid_initialise:
         ld.b r2, #SQUARE_COUNT / 3;
         move r3, r1;
@@ -24,23 +28,8 @@ Grid_initialise:
         st.w (r3), r0;
         ret;
         
-Grid_isImpossible:
-        ld.w r2, #GRID_SIZE - 1;
-        add  r2, r1;
-        ld.b r1, (r2);
-        sxt  r1; // TODO: Remove in the future
-        ret;
-        
-Grid_isComplete:
-        ld.w r2, #GRID_SIZE - 2;
-        add  r2, r1;
-        ld.b r1, #0;
-        ld.b r0, (r2);
-        bne  Grid_isComplete_return;
-        dec  r1;
-    Grid_isComplete_return:
-        ret;
-        
+// function void copyFromTo(Array source, Array target)
+//
 Grid_copyFromTo:
         move r2, r1;
         ld.w r3, (sp+2);
@@ -56,6 +45,57 @@ Grid_copyFromTo:
         st.w (r3++), r1;
         ret;
         
+// function void setSquareValue(Array grid, int value, int x, int y)
+//
+Grid_setSquareValue:
+        ld.w r2, #GRID_SIZE - 2;
+        add  r2, r1;
+        ld.b r0, (r2);
+        dec  r0;
+        st.b (r2), r0;
+    include "asm/Grid_getSquareOffset.asm";
+        add  r3, r1;
+        ld.b r0, #0;
+        ld.b r2, (sp+6);
+        bset r0, r2;
+        st.w (r3), r0;
+        ld.w r0, #0x100;
+        or   r0, r2;
+        addq r3, #2;
+        st.w (r3), r0;
+        ret;
+        
+// function boolean isImpossible(Array grid)
+//
+Grid_isImpossible:
+        ld.w r2, #GRID_SIZE - 1;
+        add  r2, r1;
+        ld.b r1, (r2);
+        sxt  r1; // TODO: Remove in the future
+        ret;
+       
+// function boolean isComplete(Array grid)
+//       
+Grid_isComplete:
+        ld.w r2, #GRID_SIZE - 2;
+        add  r2, r1;
+        ld.b r1, #0;
+        ld.b r0, (r2);
+        bne  Grid_isComplete_return;
+        dec  r1;
+    Grid_isComplete_return:
+        ret;
+                
+// function Array getSquare(Array grid, int x, int y)
+//
+Grid_getSquare:
+        nop;
+    include "asm/Grid_getSquareOffset.asm";
+        add  r1, r3;
+        ret;
+        
+// function int calculateValue(int possibilities)
+//
 Grid_calculateValue:
         ld.b r0, #1;
         ld.b r2, #0;
@@ -76,6 +116,8 @@ Grid_calculateValue:
         move r1, r2;
         ret;
 
+// function int toMask(int value)
+//
 Grid_toMask:
         ld.b r0, #0;
         bset r0, r1;
---
+++ mega_processor/asm/Grid_getSquareOffset.asm
@@ -0,0 +1,13 @@
+    // Assumes grid address is in r0
+    // Assumes x, y words under a return address on the stack
+    // Puts the square offset in r3, Clobbers r2
+        ld.b r2, (sp+2);
+        move r3, r2;
+        add  r3, r3;
+        add  r3, r3;
+        add  r3, r3;
+        add  r3, r2;
+        ld.b r2, (sp+4);
+        add  r3, r2;
+        add  r3, r3;
+        add  r3, r3;
