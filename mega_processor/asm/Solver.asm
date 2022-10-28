1d7538b64cba9bc83f6fb2c274953d9a76fdc0ca
matburton <mat.burton@red-gate.com>
2022-10-28 10:50:41 +0100

    ASM: `Grid_removeSquarePossibility` inlined
    There may be an even more aggressive way to in-line this, but I think this is probably as far as I'll go as the gains are diminishing.

--- mega_processor/asm/Solver.asm
+++ mega_processor/asm/Solver.asm
@@ -8,12 +8,57 @@ Solver_removePossibilityAt:
         addi sp, #-2;
         push r1;
         push r3;
-    include "asm/Grid_removeSquarePossibility.asm";
-    Grid_removeSquarePossibility_return:
-        bne  Solver_removePossibilityAt_value;
+        move r3, r2;
+        add  r3, r3;
+        add  r3, r3;
+        add  r3, r3;
+        add  r3, r2;
+        add  r3, r0;
+        add  r3, r3;
+        add  r3, r3;
+        add  r3, r1;
+        ld.w r0, (r3);
+        ld.w r2, (sp+0);
+        bclr r0, r2;
+        bne  Grid_removeSquarePossibility_continue;
         addi sp, #6;
         ret;
-    Solver_removePossibilityAt_value:
+    Grid_removeSquarePossibility_continue:
+        move r2, r1;
+        st.w (r3), r0;
+        addq r3, #2;
+        inc  r3;
+        ld.b r1, (r3);
+        dec  r1;
+        beq  Grid_removeSquarePossibility_impossible;
+        st.b (r3), r1;
+        dec  r1;
+        beq  Grid_removeSquarePossibility_value;
+        addi sp, #6;
+        ret;
+    Grid_removeSquarePossibility_impossible:
+        dec  r3;
+        st.w (r3), r1;
+        ld.w r0, #GRID_IMPOSSIBLE_FLAG_OFFSET;
+        add  r2, r0;
+        ld.b r0, #-1;
+        st.b (r2), r0;
+        dec  r2;
+        ld.b r0, (r2);
+        inc  r0;
+        st.b (r2), r0;
+        addi sp, #6;
+        ret;
+    Grid_removeSquarePossibility_value:
+        ld.w r1, #GRID_INCOMPLETE_SQUARE_COUNT_OFFSET;
+        add  r2, r1;
+        ld.b r1, (r2);
+        dec  r1;
+        st.b (r2), r1;
+        move r1, r0;
+        jsr  Grid_calculateValue;
+        dec  r3;
+        st.b (r3), r1;
         addi sp, #2;
         st.b (sp+2), r1;
         ld.w r1, (sp+0);
