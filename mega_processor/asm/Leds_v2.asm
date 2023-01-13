38fff9b49ca12e8380480ba577795bc5eb930859
matburton <mat.burton@red-gate.com>
2023-01-13 17:28:39 +0000

    LEDs: `renderValueAt` that trades size for speed
    This makes the lookup _much_ bigger, but maybe not too big.
    
    I might include `0` in the lookup to avoid the `dec` and assumption.

--- mega_processor/asm/Leds_v2.asm
+++ mega_processor/asm/Leds_v2.asm
@@ -1,4 +1,6 @@
 
+// Assumes value != 0
+//
 // function void renderValueAt(int y, int value, int x)
 //
 Leds_v2_renderValueAt:       
@@ -11,28 +13,20 @@ Leds_v2_renderValueAt:
         ld.b r0, (sp+2);
         add  r3, r0;
         ld.b r1, (r3);
-        add  r2, r1;     // r2 - LED pointer @ bottom
+        add  r2, r1;     // r2 - LED pointer @ top
         ld.w r3, #xToDigitLookupOffset;
         add  r3, r0;
         add  r3, r0;
         ld.w r0, (r3);
         ld.w r3, #digitLookup;
         add  r3, r0;     // r3 - Lookup pointer @ x
-        ld.w r1, (r3++); // r1 - Square mask
-    include "asm/Leds_v2_renderValueAt_maskLine.asm";
-    include "asm/Leds_v2_renderValueAt_maskLine.asm";
-    include "asm/Leds_v2_renderValueAt_maskLine.asm";
-    include "asm/Leds_v2_renderValueAt_maskLine.asm";
-        ld.w r0, (r2);
-        and  r0, r1;
-        st.w (r2), r0;   // r2 - LED pointer @ top       
         ld.b r0, (sp+4);
-        beq  Leds_v2_renderValueAt_return;
         dec  r0;
         add  r0, r0;
+        add  r0, r0;
         add  r3, r0;
         add  r0, r0;
-        add  r0, r0;      
+        add  r0, r0;
         add  r3, r0;
     include "asm/Leds_v2_renderValueAt_renderLine.asm";
         addq r2, #2;
@@ -43,5 +37,4 @@ Leds_v2_renderValueAt:
     include "asm/Leds_v2_renderValueAt_renderLine.asm";
         addq r2, #2;
     include "asm/Leds_v2_renderValueAt_renderLine.asm";
-    Leds_v2_renderValueAt_return:
         ret;
--- mega_processor/asm/Leds_v2_renderValueAt_maskLine.asm
+++
@@ -1,5 +0,0 @@
-        ld.w r0, (r2);
-        and  r0, r1;
-        st.w (r2), r0;
-        addq r2, #-2;
-        addq r2, #-2;
