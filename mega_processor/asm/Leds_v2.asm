d4e3f2688038a1c1acf9006e3139011347a30973
matburton <mat.burton@red-gate.com>
2023-01-19 10:17:45 +0000

    LEDs: Better looking v2 `renderGrid`
    Which wipes each row just before it renders it.
    Slightly less performant but looks much less crappy.
    
    Maybe I can remove the old rendering now?

--- mega_processor/asm/Leds_v2.asm
+++ mega_processor/asm/Leds_v2.asm
@@ -49,29 +49,10 @@ Leds_v2_renderValueAt_skip_precondition:
 Leds_v2_renderGrid:
         ld.b r0, gridRenderDisableCount;
         bne  Leds_v2_renderValueAt_return;
-        ld.w r0, #SQUARE_COUNT * SQUARE_SIZE + 2;
-        add  r1, r0;
-        push r1;
-        ld.w r2, #LED_START + 4;
-        ld.w r3, #startupLeds + 4;
-        ld.b r1, #114 / 6;
-    Leds_v2_renderGrid_wipe_loop:
-        ld.w r0, (r3++);
-        st.w (r2++), r0;
-        ld.w r0, (r3++);
-        st.w (r2++), r0;
-        ld.w r0, (r3++);
-        st.w (r2++), r0;
-        ld.w r0, (r3++);
-        st.w (r2++), r0;
-        ld.w r0, (r3++);
-        st.w (r2++), r0;
-        ld.w r0, (r3++);
-        st.w (r2++), r0;
-        dec  r1;
-        bne  Leds_v2_renderGrid_wipe_loop;
-        pop  r2;     // square value ptr
+        ld.w r2, #SQUARE_COUNT * SQUARE_SIZE + 2;
+        add  r2, r1; // square value ptr
         ld.b r0, #8; // y
+        jsr  Leds_v2_wipeY;
         ld.b r3, #9; // x
     Leds_v2_renderGrid_square_loop:
         addq r2, #-2;
@@ -80,6 +61,7 @@ Leds_v2_renderGrid:
         bpl  Leds_v2_renderGrid_square_loop_same_y;
         dec  r0;
         bmi  Leds_v2_renderGrid_return;
+        jsr  Leds_v2_wipeY;
         ld.b r3, #8;
     Leds_v2_renderGrid_square_loop_same_y:
         ld.b r1, (r2);
@@ -93,4 +75,39 @@ Leds_v2_renderGrid:
         pop  r2;
         jmp  Leds_v2_renderGrid_square_loop;
     Leds_v2_renderGrid_return:
+        ret;
+        
+// function void wipeY(int y)
+//
+// y in r0, r0 and r2 retained
+//
+Leds_v2_wipeY:
+        push r2;
+        ld.w r3, #ledStartLookupFromY;
+        add  r3, r0;
+        add  r3, r0;
+        ld.w r1, (r3);
+        move r3, r1;
+        ld.w r2, #startupLeds + 4;
+        ld.w r1, (r2++);
+        st.w (r3++), r1;
+        ld.w r1, (r2++);
+        st.w (r3++), r1;
+        ld.w r1, (r2++);
+        st.w (r3++), r1;
+        ld.w r1, (r2++);
+        st.w (r3++), r1;
+        ld.w r1, (r2++);
+        st.w (r3++), r1;
+        ld.w r1, (r2++);
+        st.w (r3++), r1;
+        ld.w r1, (r2++);
+        st.w (r3++), r1;
+        ld.w r1, (r2++);
+        st.w (r3++), r1;
+        ld.w r1, (r2++);
+        st.w (r3++), r1;
+        ld.w r1, (r2++);
+        st.w (r3++), r1;
+        pop  r2;
         ret;
